fca07016d29b8ea16bd2e5c1796aed37d81bcdfb
root <you@example.com>
2021-10-07 08:04:06 +0100

    add new project

--- cnf.py
+++ cnf.py
@@ -31,7 +31,7 @@ new_driver_path = '/usr/bin/geckodriver13'
 new_binary_path = '/root/EXTRAT/firefox/firefox'
 
 
-user_agent = random.choice(user_agent_list)
+
 
 
 
@@ -20399,3 +20399,4 @@ user_agent_list = [
 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.2; Trident/4.0; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729)',
 'Nokia5130c-2/2.0 (07.97) Profile/MIDP-2.1 Configuration/CLDC-1.1'
 ]
+user_agent = random.choice(user_agent_list)
